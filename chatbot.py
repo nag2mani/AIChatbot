7d249b51258fefc813137c8630603632cce944ff
Nagmani kumar <nag2mani@gmail.com>
2023-12-02 21:54:31 +0530

    added stremlit code

---
+++ chatbot.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title(" 🤖 Chatbot Made by Team BiteBase")
+st.subheader(" Chat and Explore with AI :-")
+query = st.text_input("Hey there! Ask your question ")
+
+st.write(query)
