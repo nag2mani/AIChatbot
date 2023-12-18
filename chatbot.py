f5ab0a5f90fe8ace632bf46036581d8879af46a0
Nagmani kumar <nag2mani@gmail.com>
2023-12-18 18:44:53 +0530

    satvik sir file added

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from streamlit_chat import message
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+
+OPENAI_API_KEY = "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"
+
+
+# Initialize ChatOpenAI and ConversationChain
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
+
 st.title(" 🤖 Chatbot Made by Team BiteBase")
 st.subheader(" Chat and Explore with AI :-")
 query = st.text_input("Hey there! Ask your question ")
 
-st.write(query)
+if query:
+    with st.spinner("typing..."):
+        response = conversation.predict(input=query)
+        st.write(response)
